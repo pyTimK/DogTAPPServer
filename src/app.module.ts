fde2b3f28a655f828d63617cff92fbfc235ebb39
pyTimK <45229539+pyTimK@users.noreply.github.com>
2023-02-05 23:32:13 +0800

    added fileservice in appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { FileService } from './file/file.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), CacheModule.register()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FileService],
 })
 export class AppModule {}
